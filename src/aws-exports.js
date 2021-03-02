0cd99f6e166f03718af9660b9aab8d2727e0ec79
Gonzalo Rojas Alvarez <39566250+Sisterno@users.noreply.github.com>
2021-03-02 09:26:23 -0500

    Update aws-exports.js

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,10 +3,9 @@
 
 const awsmobile = {
     "aws_project_region": "us-east-1",
-    "aws_cognito_identity_pool_id": "us-east-1:df18340b-8d78-4f7b-ac41-34f458681751",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_ZeRqX3qhp",
-    "aws_user_pools_web_client_id": "3ap9m0leodfrknf58a6muvrv14",
+    "aws_user_pools_id": "us-east-1_NuMjx6T7e",
+    "aws_user_pools_web_client_id": "29eqqq27i98rnqojt0pu93ni7l",
     "oauth": {}
 };
 
